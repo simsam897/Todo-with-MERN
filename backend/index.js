d3e38d8e405cd9d2e0737f62d49c49dde9e031fe
simsam897 <simma013s@gmail.com>
2026-05-16 22:51:54 +0530

    connect backend with mongodb database

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,22 @@
+import dotenv, { configDotenv } from "dotenv";
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+
+import { DB_NAME } from "./constants.js";
 const app = express();
 
+dotenv.config({ path: "./.env" });
 app.use(express.json());
 app.use(cors());
 
+mongoose
+  .connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log(err));
+const port = process.env.PORT;
 app.get("/api/test", (req, res) => {
   res.json({ message: "Backend is working!" });
 });
 
-app.listen(5000, () => console.log("Server running at port 5000"));
-
-
+app.listen(5000, () => console.log("Server running at ${port}"));
