a4813ea50cc7c18a84eca56193ffd60b87fc36c2
simsam897 <simma013s@gmail.com>
2026-05-16 22:50:26 +0530

    connect frontend wiht backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,11 @@ import { useEffect } from "react";
 
 
 function App() {
-
+  useEffect(() => {
+    fetch("http://localhost:5000/api/test")
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, []);
 
   return (
     <>
