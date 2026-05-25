fa90c8bb91a965034d547cf54f6a4d063aeb824d
simsam897 <simma013s@gmail.com>
2026-05-25 20:13:52 +0530

    install tailwindcss and config  in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-
-  server: {
-    proxy: {
-      "/api": "http://localhost:5000",
-    },
-  },
-  plugins: [react()],
+  plugins: [tailwindcss()],
 });
