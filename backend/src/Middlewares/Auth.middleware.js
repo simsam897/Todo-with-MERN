d91738a6c44d95d6f33f54f580d52cbb6c0d61c7
simsam897 <simma013s@gmail.com>
2026-08-07 20:36:24 +0530

    add auth middleware

---
+++ backend/src/Middlewares/Auth.middleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  try {
+    const token = req.cookies.access_token;
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid token",
+    });
+  }
+};
