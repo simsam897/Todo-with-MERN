4006570ef812f97c7f5a4d6e9accb3b6f9133689
simsam897 <simma013s@gmail.com>
2026-08-08 08:32:17 +0530

    fix issue in auth middleware related to verifytoken for deployment

--- backend/src/Middlewares/Auth.middleware.js
+++ backend/src/Middlewares/Auth.middleware.js
@@ -2,22 +2,26 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   try {
-    const token = req.cookies.access_token;
+    // Get JWT from the httpOnly cookie
+    const token = req.cookies?.access_token;
 
+    // No token
     if (!token) {
       return res.status(401).json({
-        message: "Unauthorized",
+        message: "Unauthorized - token missing",
       });
     }
 
+    // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+    // Store decoded user information
     req.user = decoded;
 
     next();
   } catch (error) {
     return res.status(401).json({
-      message: "Invalid token",
+      message: "Unauthorized - invalid or expired token",
     });
   }
 };
