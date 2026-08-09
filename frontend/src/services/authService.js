15ff3a6652480056c33add4ba9b802fef345496b
simsam897 <simma013s@gmail.com>
2026-08-09 22:08:13 +0530

    fix error related to deployment issue in frontend

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,14 +1,18 @@
 import API from "./service";
 
-export const signupUser =  (data) => 
-   = API.post("/user/signup", data);
-  
+export const signupUser = async (data) => {
+  const response = await API.post("/user/signup", data);
+  return response.data;
+};
 
 export const signinUser = (data) => {
   return API.post("/user/signin", data);
 };
 
-export const signoutUser = () => API.post("/user/signout");
+export const signoutUser = () => {
+  return API.post("/user/signout");
+};
 
-export const updateProfile = (formData) =>
-  API.put("/user/update-profile", formData);
+export const updateProfile = (formData) => {
+  return API.put("/user/update-profile", formData);
+};
