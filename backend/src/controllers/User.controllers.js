13dbe0bcb87f26a9dad8071c9c2601a9b5c18ecd
simsam897 <simma013s@gmail.com>
2026-08-11 17:47:02 +0530

    fix a cookie related mistake

--- backend/src/controllers/User.controllers.js
+++ backend/src/controllers/User.controllers.js
@@ -117,7 +117,7 @@ export const signin = async (req, res) => {
     res.cookie("access_token", token, {
       httpOnly: true,
       secure: isProduction,
-     sameSite: isProduction ? "lax" : "none",
+    sameSite: isProduction ? "none" : "lax",
       path: "/",
       maxAge: 24 * 60 * 60 * 1000,
     });
@@ -149,7 +149,7 @@ export const signout = (req, res) => {
     res.clearCookie("access_token", {
       httpOnly: true,
       secure: isProduction,
-      sameSite: isProduction ? "none" : "lax",
+     sameSite: isProduction ? "none" : "lax",
       path: "/",
     });
 
