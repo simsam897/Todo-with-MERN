bf119c03ff3793b4a90db2f5de4b26e7cca043bf
simsam897 <simma013s@gmail.com>
2026-08-15 17:14:21 +0530

    fix error related to deployment related to upload profile in multer

--- backend/src/Middlewares/multer.middleware.js
+++ backend/src/Middlewares/multer.middleware.js
@@ -3,7 +3,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/temp");
+    cb(null, "/tmp");
   },
 
   filename: function (req, file, cb) {
